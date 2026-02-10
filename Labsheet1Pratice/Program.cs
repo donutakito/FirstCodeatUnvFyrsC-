0a5a2ab3d3f8ada0317d00e68d911b6d0d21c23b
JustDonutwithMyMimi <victorakito303549@gmail.com>
2026-02-10 18:43:19 +0700

    Labsheet1

---
+++ Labsheet1Pratice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labsheet1Pratice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Name:\t\t\tNut Lungdang");
+            Console.WriteLine("Student ID:\t\t2543110631"); 
+            Console.WriteLine("Department:\t\tInformation of teacnoology");
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
